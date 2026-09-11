ca79a44a2b8ef9e4d1d123fa2a825fa4b5953ef3
Changhwan Kim <kimch061279@gmail.com>
2026-09-11 21:13:45 +0900

    feat: repository.py; initialize 메서드 추가 및 INIT 기능 구현

--- repository.py
+++ repository.py
@@ -8,6 +8,9 @@ class Repository:
     def __init__(self) -> None:
         self._commit_counter: int = 0
         self.commits: dict[str, Commit] = {} # 키는 커밋 ID, 값은 Commit 객체
+        self.branches: dict[str, str | None] = {} # 각 브랜치가 가리키는 커밋 ID
+        self.head: str | None = None # 현재 브랜치 이름
+        self.current_user: str | None = None
 
     def _generate_commit_hash(self) -> str:
         """현재 세션에서 사용하지 않은 커밋 ID를 생성해 반환한다."""
@@ -18,6 +21,16 @@ class Repository:
             self._commit_counter += 1
             if tmp_commit_id not in self.commits:
                 return tmp_commit_id
+
+    def initialize(self, user_name: str) -> None:
+        """main 브랜치, HEAD, 현재 사용자를 설정한다."""
+        # 다시 INIT를 하면 main의 커밋 위치가 None으로 바뀌는 등 여러 이슈가 생길 수 있으니 일단은 막자.. 근데 나중에 다시 생각해봐야 할 것 같음
+        if self.current_user != None:
+            raise ValueError("이미 초기화가 완료된 Repository입니다.")
+
+        self.current_user = user_name
+        self.branches["main"] = None # main 브랜치는 있지만 아직 커밋이 없어요
+        self.head = "main"
 
     def store_commit(self, commit: Commit) -> None:
         """커밋을 저장한다. 이미 사용 중인 ID는 허용하지 않는다."""
