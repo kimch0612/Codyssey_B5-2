e3fe9ad4da2628f1efb000d7fafe4495e7438d34
Changhwan Kim <kimch061279@gmail.com>
2026-09-11 16:18:16 +0900

    feat: Commit class 및 생성자 함수 구현

--- commit.py
+++ commit.py
@@ -1,1 +1,19 @@
 # 커밋 한 개의 정보 표현 담당
+from datetime import datetime
+
+
+class Commit:
+    """커밋 한 개의 메타데이터와 부모 관계를 표현한다."""
+    def __init__(
+        self,
+        commit_hash: str,
+        message: str,
+        author: str,
+        timestamp: datetime,
+        parents: list[str],
+    ) -> None:
+        self.hash = commit_hash
+        self.message = message
+        self.author = author
+        self.timestamp = timestamp
+        self.parents = parents
