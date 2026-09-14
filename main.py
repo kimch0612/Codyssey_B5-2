586d2109faa386c9d2022ea29d7ed64d7310f9f0
Changhwan Kim <kimch061279@gmail.com>
2026-09-14 12:28:01 +0900

    feat: main.py; 함수 구현 (format_log)

--- main.py
+++ main.py
@@ -7,4 +7,16 @@ def format_log(
     ordered_ids: list[str],
 ) -> str:
     """주어진 ID 순서대로 커밋 정보를 로그 문자열로 구성한다."""
-    pass
+    result = []
+
+    for hash in ordered_ids:
+        commit = commits[hash]
+        c_author = commit.author
+        c_timestamp = commit.timestamp
+        c_message = commit.message
+
+        tmp_result = f"commit {hash} ({c_author}, {c_timestamp})\n{c_message}"
+        result.append(tmp_result)
+    
+    if not result: return ""
+    else: return "\n".join(result)
