b2347367829423c968c0b76230dcd4e3e918265b
Changhwan Kim <kimch061279@gmail.com>
2026-09-14 15:14:04 +0900

    feat: repository.py; search_by_author 메서드 구현

--- repository.py
+++ repository.py
@@ -91,6 +91,16 @@ class Repository:
 
         return matched_commits
 
+    def search_by_author(self, author: str) -> list[Commit]:
+        """작성자 색인으로 찾은 ID들을 실제 Commit 객체 목록으로 반환한다."""
+        matched_ids = self.commit_index.find_by_author(author)
+        matched_commits: list[Commit] = []
+
+        for id in matched_ids:
+            matched_commits.append(self.get_commit(id))
+
+        return matched_commits
+
 # ///////////////////////// Branch Section /////////////////////////
 
     def create_branch(self, branch_name: str) -> None:
